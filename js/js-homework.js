2e7cc415e193759b67ba2fb46632136f19c6881e
Natalja <tasia@inbox.lt>
2023-06-04 23:24:39 +0300

    js- 1,2 done

--- js/js-homework.js
+++ js/js-homework.js
@@ -1,10 +1,26 @@
 /*
 1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console*/
+const a = 1; 
+const b = 3; 
+const c = 6; 
+console.log(a);
+console.log(b);
+console.log(c);
 /*2.Sukurti 3 kintamuosius su teksto tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console*/
+
+const element1 = "kompiuteris"; 
+const element2 = "stalas"; 
+const element3 = "kede"; 
+console.log(element1);
+console.log(element2);
+console.log(element3);
+
 /*3.Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console*/
+
+
 /*4.Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
 Po kiekvieno jų inicijavimo, išvesti į console
 */
