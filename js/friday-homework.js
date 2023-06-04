fdd7cc218781d49a0a7a640ae4c285d88ff8381f
Natalja <tasia@inbox.lt>
2023-06-04 19:48:00 +0300

    1 homework

---
+++ js/friday-homework.js
@@ -0,0 +1,40 @@
+const { log } = require("console");
+
+/*
+1. Sukurti du kintamuosius. 
+Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis). 
+Atspausdinti trumpesnį stringą.*/
+
+	function initialai(vardas, pavarde) {
+        return vardas + "." + pavarde
+    }
+	
+	const init = initialai("Bruce", "Willis");
+
+    console.log(init);
+/*
+2.Sukurkite 4 kintamuosius, kurie saugotų jūsų vardą, pavardę, gimimo metus ir šiuos metus (nebūtinai tikrus). 
+Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių ir naudodamas vardo ir pavardės kintamuosius atspausdintų tokį sakinį :
+"Aš esu Vardenis Pavardenis. Man yra XX metai(ų)".*/
+	
+	
+	
+/*
+3.Sukurti du kintamuosius. 
+Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. 
+Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių. 
+Jį atspausdinti.*/
+	
+	
+	
+/*
+4.Sukurti kintamąjį su stringu: “Once upon a time in hollywood”. 
+Jame visas “o” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  
+Rezultatą atspausdinti.*/
+	
+	
+	
+/*
+5.Sukurkite keturis kintamuosius kuriems sugeneruokite atsitiktines reikšmes nuo 0 iki 2. 
+Suskaičiuokite kiek yra nulių, vienetų ir dvejetų. 
+*/
