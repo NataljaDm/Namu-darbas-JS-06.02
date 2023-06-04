7e1d472b2252796dd1751d3c9618c43b0720f927
Natalja <tasia@inbox.lt>
2023-06-04 23:10:55 +0300

    js-homework

---
+++ js/js-homework.js
@@ -0,0 +1,10 @@
+/*
+1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+/*2.Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+/*3.Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console*/
+/*4.Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
