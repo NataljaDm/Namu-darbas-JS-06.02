4218161d11f586334851c613c148252e350e5133
Natalja <tasia@inbox.lt>
2023-06-07 12:06:49 +0300

    paskaitos uzd

--- js/for-homework.js
+++ js/for-homework.js
@@ -1,6 +1,6 @@
 /*Ciklo for panaudojimas*/
 
-/*1.Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
+/*-1.Suskaičiuoti ką gausime susumavus skaičius intervale tarp (imtinai):
 0 … 0
 0 … 4
 0 … 100
@@ -44,8 +44,8 @@ console.log(sum);
 for (let i = -70; i <= 30; i++) {
     sum += i
     console.log(sum);
-}
-console.log(sum);*/
+
+console.log(sum);
 
 
 /*2.panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
@@ -66,6 +66,8 @@ Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 viene
 Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
 Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai.*/
 
+
+
 /*
 4. // Kiek duotas masyvas turi ieskomo skaiciaus atveju (kiekio)?
 // m1:1 -> 3
@@ -76,9 +78,81 @@ Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 viene
 // m2:3 -> 1
 // m4:1 -> 5
 // m4:2 -> 3
-// m4:3 -> 2
+// m4:3 -> 2*/
 
 const masyvas1 = [1, 1, 1];
+let vienas = 0;
+for (let i = 0; i < masyvas1.length; i++){
+     if(masyvas1[i] === 1 ){
+      vienas++;} }
+    
+console.log(`m1:1=`, vienas)
+
 const masyvas2 = [1, 2, 2, 3];
+let x = 0;
+let du = 0;
+let tris = 0;
+for (let i = 0; i < masyvas2.length; i++) {
+   if(masyvas2[i] === 1){
+    x++; } 
+else if (masyvas2[i] === 2) {
+du++;   
+} 
+else if (masyvas2[i] === 3) {
+    tris++;   
+    } 
+}
+ console.log(`m2:1=`, x);
+ console.log(`m2:2=`, du);
+ console.log(`m2:3=`, tris);
+
 const masyvas3 = [5, 4, 3, 2, 1];
-const masyvas4 = [5, 4, 3, 2, 1, 1, 1, 1, 1, 2, 2, 3];*/
+let a = 0;
+let b = 0;
+let c = 0;
+let d = 0;
+let e = 0;
+for (let i = 0; i < masyvas3.length; i++) {
+    if(masyvas3[i] === 1){
+     a++; } 
+ else if (masyvas3[i] === 2) {
+ b++;   
+ } 
+ else if (masyvas3[i] === 3) {
+     c++;   
+     } 
+     else if (masyvas3[i] === 4) {
+        d++;   
+        } 
+        else if (masyvas3[i] === 5) {
+            e++;   
+            } }
+ console.log(`m3:1=`, a);
+ console.log(`m3:2=`, b);
+ console.log(`m3:3=`, c);
+ console.log(`m3:4=`, d);
+ console.log(`m3:5=`, e);
+
+const masyvas4 = [5, 4, 3, 2, 1, 1, 1, 1, 1, 2, 2, 3];
+let q = 0;
+let w = 0;
+let r = 0;
+let t = 0;
+let y = 0;
+for (let i = 0; i < masyvas4.length; i++) {
+    if(masyvas4[i] === 1){
+     q++; } 
+ else if (masyvas4[i] === 2){
+ w++; } 
+ else if (masyvas4[i] === 3){
+     r++; } 
+else if (masyvas4[i] === 4){
+        t++; } 
+ else if (masyvas4[i] === 5){
+            y++; } }
+ console.log(`m3:1=`, q);
+ console.log(`m3:2=`, w);
+ console.log(`m3:3=`, r);
+ console.log(`m3:4=`, t);
+ console.log(`m3:5=`, y);
+  
